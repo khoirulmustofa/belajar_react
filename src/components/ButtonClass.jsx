6cf3c0f8e7f185655b8537e54b5eac6fd5daf87e
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2022-11-07 14:27:47 +0700

    Update ButtonClass.jsx

--- src/components/ButtonClass.jsx
+++ src/components/ButtonClass.jsx
@@ -9,6 +9,7 @@ class ButtonClass extends Component {
                 <button className='bg-slate-200 py-2 px-4 rounded'> Button CLass</button>
                 <br />
                 <button className='bg-slate-200 py-2 px-4 rounded'> {this.props.text}</button>
+                <button className='bg-slate-200 py-2 px-4 rounded'> {this.props.test_test ? this.props.test_test : "Button Class Default"}</button>
             </div>
         );
     }
